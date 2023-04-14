b503886b38bf2a8fb785353a7fb4a0b9c63e2a37
SJ Chen <shangjuc@gmail.com>
2023-04-14 14:47:18 +0800

    Add chatHistory

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,21 +4,24 @@ import Link from 'next/link';
 export default function Navbar() {
 
     return (
-        <div className="flex w-full fiexd border-b p-4 mb-10">
-            <ul className=' flex'>
-                <li className=' mr-3'>
-                    <Link href="/">Home</Link>
-                </li>
-                <li className=' mr-3'>
-                    <Link href="/word-cloud">WordCloud</Link>
-                </li>
-                <li className=' mr-3'>
-                    <Link href="/chatgpt">ChatGPT</Link>
-                </li>
-                <li className=' mr-3'>
-                    <Link href="/post/hello">Blog Post</Link>
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="flex w-full fixed border-b p-4 top-0 bg-slate-600">
+                <ul className=' flex'>
+                    <li className=' mr-3'>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className=' mr-3'>
+                        <Link href="/word-cloud">WordCloud</Link>
+                    </li>
+                    <li className=' mr-3'>
+                        <Link href="/chatgpt">ChatGPT</Link>
+                    </li>
+                    <li className=' mr-3'>
+                        <Link href="/post/hello">Blog Post</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="fornav w-full mb-32"></div>
+        </>
     )
 }
--- src/pages/chatgpt.tsx
+++ src/pages/chatgpt.tsx
@@ -6,58 +6,92 @@ const title_msg = `ChatGPT test`;
 export default function ChatGPT() {
 
     const [userInput, setUserInput] = useState('');
-    const [aiAnswer, setAiAnswer] = useState('輸入問題以獲得答案');
+    const [chatHistory, setChatHistory] = useState([{ user_input: '', ai_answer: '' }]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    function get_anwser(){
+
+    function get_anwser() {
         console.log(userInput);
-        let params = {q: userInput}
-        setAiAnswer('。。。')
+        if(!userInput) return;
+        let params = { q: userInput }
+        if(isLoading) return;
+        setIsLoading(true);
+        setUserInput('');
 
         fetch(`http://localhost:5566/api/chatgpt?q=${userInput}`)
-        .then(res=>{
-            // console.log(res)
-            return res.json();
-        })
-        .then(myjson=>{
-            console.log(myjson)
-            setAiAnswer(myjson.answer)
-        })
+            .then(res => {
+                // console.log(res)
+                return res.json();
+            })
+            .then(myjson => {
+                console.log(myjson)
+                if (typeof myjson?.answer === 'string') {
+                    setIsLoading(false);
+                    let temp_history = chatHistory;
+                    // temp_history.push({ user_input: userInput, ai_answer: myjson?.answer })
+                    temp_history[temp_history.length - 1].ai_answer = myjson.answer;
+                    setChatHistory(temp_history);
+                } else {
+                    setIsLoading(false);
+                }
+            })
     }
 
-    function handleChange(event:any){
+    function handleChange(event: any) {
         setUserInput(event.target.value);
 
     }
-    function handleKeyDown(event:any){
+    function handleKeyDown(event: any) {
         // console.log(e);
         // setUserInput(e.target.value);
         if (event.key === 'Enter') {
-            get_anwser()
+            let temp_history = chatHistory;
+            temp_history.push({ user_input: userInput, ai_answer: '(...)' })
+            setChatHistory(temp_history);
+            get_anwser();
+            event.target.value = '';
         }
 
     }
 
+
+
     return (
-        <>  
+        <>
             <Head>
                 <title>{title_msg}</title>
             </Head>
-            <Navbar/>
+            <Navbar />
             <div className=" flex w-full justify-center items-center">
-                <div className="card flex flex-wrap w-96 h-96 p-10 border rounded border-white">
+                <div className="card flex flex-wrap w-1/2 h-auto p-10 border rounded border-white">
+                    <h1>我婆模擬器</h1>
+                    {chatHistory.map((item, index) =>
 
-                    <div className="user-input flex w-full justify-center items-center">
-                        <input type="text" className=" text-gray-500 mr-4 h-10 rounded px-2" 
-                        onChange={handleChange} 
-                        onKeyDown={handleKeyDown}/>
-                        <button className="border p-2 rounded" onClick={get_anwser} onKeyDown={get_anwser}>Submit</button>
+                        item.user_input &&
+                        <div key={index} className="w-full border-b py-10" >
+                            <p>Q: {item.user_input}</p>
+                            <p>琴葉: {item.ai_answer}</p>
+
+                        </div>
+                    )}
+                    {/* {isLoading && <div className="w-full">Loading...</div>} */}
+                    <div className="user-input flex flex-wrap w-full justify-center items-center py-10">
+                        <input type="text" placeholder="輸入想問琴葉的問題" className=" w-full text-gray-500 mb-5 h-10 rounded px-2"
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown} />
+                        <button className="w-full border p-2 rounded" onClick={get_anwser} onKeyDown={get_anwser}>送出</button>
                     </div>
-                    <div className="answer flex w-full justify-center border-t py-10">
-                        
+                    
+
+                    
+                    {/* <div className="answer flex w-full justify-center border-t py-10">
+
                         <p>{aiAnswer}</p>
-                    </div>
+                    </div> */}
+
+
                 </div>
-                
+
             </div>
         </>
     )
