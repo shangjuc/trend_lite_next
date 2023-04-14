faaf03d1c74d60bbd13002c81ea398b6a1bb5afc
SJ Chen <shangjuc@gmail.com>
2023-04-14 17:26:02 +0800

    Add interface and multi role at chatgpt

--- src/pages/chatgpt.tsx
+++ src/pages/chatgpt.tsx
@@ -3,22 +3,45 @@ import Head from "next/head";
 import Navbar from '@/components/navbar/navbar'
 const title_msg = `ChatGPT test`;
 
+interface I_Chat {
+    user_input: string;
+    ai_answer: string;
+    is_loading: boolean;
+}
+interface I_ChatHistoryData {
+    kotoha: I_Chat[]|[];
+    gp: I_Chat[]|[];
+    ai: I_Chat[]|[];
+}
+
 export default function ChatGPT() {
 
-    const [userInput, setUserInput] = useState('');
-    const [chatHistory, setChatHistory] = useState([{ user_input: '', ai_answer: '' }]);
-    const [isLoading, setIsLoading] = useState(false);
+    const [userInput, setUserInput] = useState<string>('');
+    const [aiRole, setAiRole] = useState<string>('kotoha');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [chatHistoryData, setChatHistoryData] = useState<I_ChatHistoryData>({
+        'kotoha': [],
+        'gp': [],
+        'ai': []
+    })
 
 
-    function get_anwser() {
-        console.log(userInput);
-        if(!userInput) return;
-        let params = { q: userInput }
-        if(isLoading) return;
+    function get_anwser(user_input:string, ai_role:string) {
+        console.log(user_input);
+        if (!user_input) return;
+        // let params = { q: user_input }
+        // if (isLoading) return;
+        let temp_history:I_Chat[] = chatHistoryData[ai_role as keyof I_ChatHistoryData];
+        if(temp_history[temp_history.length - 1].is_loading) return;
+        temp_history[temp_history.length - 1].is_loading = true;
+        setChatHistoryData({
+            ...chatHistoryData,
+            [ai_role as keyof I_ChatHistoryData]: temp_history
+        });
         setIsLoading(true);
         setUserInput('');
 
-        fetch(`http://localhost:5566/api/chatgpt?q=${userInput}`)
+        fetch(`http://localhost:5566/api/chatgpt?q=${user_input}&role=${ai_role}`)
             .then(res => {
                 // console.log(res)
                 return res.json();
@@ -27,10 +50,12 @@ export default function ChatGPT() {
                 console.log(myjson)
                 if (typeof myjson?.answer === 'string') {
                     setIsLoading(false);
-                    let temp_history = chatHistory;
-                    // temp_history.push({ user_input: userInput, ai_answer: myjson?.answer })
                     temp_history[temp_history.length - 1].ai_answer = myjson.answer;
-                    setChatHistory(temp_history);
+                    temp_history[temp_history.length - 1].is_loading = false;
+                    setChatHistoryData({
+                        ...chatHistoryData,
+                        [ai_role as keyof I_ChatHistoryData]: temp_history
+                    });
                 } else {
                     setIsLoading(false);
                 }
@@ -42,17 +67,35 @@ export default function ChatGPT() {
 
     }
     function handleKeyDown(event: any) {
+        
         // console.log(e);
         // setUserInput(e.target.value);
         if (event.key === 'Enter') {
-            let temp_history = chatHistory;
-            temp_history.push({ user_input: userInput, ai_answer: '(...)' })
-            setChatHistory(temp_history);
-            get_anwser();
+            let ai_role = aiRole;
+            let user_input = userInput;
+            if (!user_input) return;
+            let temp_history:I_Chat[] = chatHistoryData[ai_role as keyof I_ChatHistoryData];
+            if(temp_history[temp_history.length - 1]?.is_loading) return;
+
+            temp_history.push({ user_input: user_input, ai_answer: '(...)', is_loading:false });
+            // chatHistoryData[temp_ai_role as keyof I_ChatHistoryData] = temp_history;
+            setChatHistoryData({
+                ...chatHistoryData,
+                [ai_role as keyof I_ChatHistoryData]: temp_history
+            });
+            // console.log(chatHistoryData)
+            get_anwser(user_input, ai_role);
             event.target.value = '';
         }
 
     }
+
+    const selectAiRole = (role:string)=>{
+        // let temp_history:I_Chat[] = chatHistoryData[role as keyof I_ChatHistoryData];
+        // console.log(chatHistoryData)
+        setAiRole(role);
+    }
+
 
 
 
@@ -63,27 +106,34 @@ export default function ChatGPT() {
             </Head>
             <Navbar />
             <div className=" flex w-full justify-center items-center">
-                <div className="card flex flex-wrap w-1/2 h-auto p-10 border rounded border-white">
-                    <h1>我婆模擬器</h1>
-                    {chatHistory.map((item, index) =>
+                <div className="card flex flex-wrap justify-center w-1/2 h-auto p-10 border rounded border-white">
+                    <h1 className=" text-center mb-3">{aiRole === 'kotoha' ? '我婆琴葉' : aiRole.toUpperCase()}模擬器</h1>
+                    <div className="btn-container flex w-full">
+                        {['kotoha', 'gp', 'ai'].map((item,idx)=>{
+                            return <button key={idx} className={ (aiRole===item? ' bg-slate-500': '') + " border rounded px-5 py-1 mr-1"}  onClick={() => { selectAiRole(item) }}>{item.toUpperCase()}</button>
+                        })}
+                    </div>
 
-                        item.user_input &&
+
+                    {chatHistoryData[aiRole as keyof I_ChatHistoryData].map((item, index) =>
+
+                        true &&
                         <div key={index} className="w-full border-b py-10" >
                             <p>Q: {item.user_input}</p>
-                            <p>琴葉: {item.ai_answer}</p>
+                            <p>{aiRole.toLocaleUpperCase()}: {item.ai_answer}</p>
 
                         </div>
                     )}
                     {/* {isLoading && <div className="w-full">Loading...</div>} */}
                     <div className="user-input flex flex-wrap w-full justify-center items-center py-10">
-                        <input type="text" placeholder="輸入想問琴葉的問題" className=" w-full text-gray-500 mb-5 h-10 rounded px-2"
+                        <input type="text" placeholder="輸入想問的問題" className=" w-full text-gray-500 mb-5 h-10 rounded px-2"
                             onChange={handleChange}
                             onKeyDown={handleKeyDown} />
-                        <button className="w-full border p-2 rounded" onClick={get_anwser} onKeyDown={get_anwser}>送出</button>
+                        <button className="w-full border p-2 rounded" onClick={()=>get_anwser(userInput, aiRole)}>送出</button>
                     </div>
-                    
 
-                    
+
+
                     {/* <div className="answer flex w-full justify-center border-t py-10">
 
                         <p>{aiAnswer}</p>
