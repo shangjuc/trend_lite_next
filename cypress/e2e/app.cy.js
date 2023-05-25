a422c20e2d941b1c8c29a748db08c79e27910d49
shangjuc <shangjuc@gmail.com>
2023-05-25 16:42:44 +0800

    Add cypress testing

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,15 @@
+describe('Navigation', () => {
+    it('should navigate to the about page', () => {
+        // Start from the index page
+        cy.visit('http://localhost:4500/');
+
+        // Find a link with an href attribute containing "about" and click it
+        cy.get('a[href*="/chatgpt/ai"]').click();
+
+        // The new url should include "/about"
+        cy.url().should('include', '/chatgpt/ai');
+
+        // The new page should contain an h1 with "About page"
+        cy.get('h1').contains('AI模擬');
+    });
+});
